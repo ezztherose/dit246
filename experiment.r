7ee5124d91eeceff9c359510ca1157e5e81ffbaa
Roseri <erikrosvall96@gmail.com>
2022-09-30 11:24:30 +0200

    adding done data

---
+++ experiment.r
@@ -0,0 +1,17 @@
+library(stringr)
+
+indata <- read.csv("./data/data_autumn2020.csv", header = TRUE, sep=";")
+#print(head(indata)) # sanity check
+
+# setting the correct categories?
+ME <- indata[indata$category == "ME",]
+LE <- indata[indata$category == "LE",]
+NT <- indata[indata$technique == "NT",]
+OT <- indata[indata$technique == "OT",]
+
+# mappling correlating data with eachother?
+NT_LE <- LE[LE$technique == "NT",]
+OT_LE <- LE[LE$technique == "OT",] # missing sub. 36?
+NT_ME <- ME[ME$technique == "NT",]
+OT_ME <- ME[ME$technique == "OT",]
+
